6c4c5c0339f4b7f19cbded4037403bc052734a8c
BertyBer <chiskyxl@hotmail.com>
2021-04-02 10:20:23 +0100

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
+# spam = 40
+# spam
+# print (spam)
 
+# print ("alice"+ "bob")
+# print(len("Alice"))
 
-
-
+print("hello what is your name")
+myName = input()
+print("Really good to see you here, " + myName)
+print("Fun fact, the length of your name is:")
+print(len(myName))
+print("what is your age?")
+myAge = input()
+print ("Do you know you will be " + str(int(myAge)+6)+ " in six years time?")
+print("I know, mind blowing!! " + myName)
